1ef97502ed9cc127f9f9b9ec480d8fbacda6eef0
Catherine Winters <catherine@catherinewinters.com>
2018-12-10 12:36:51 -0800

    created proof-of-concept

--- assets/js/star-wipe.js
+++ assets/js/star-wipe.js
@@ -1,1 +1,41 @@
-dfs
+$(document).ready(function() {
+  buildDoubles();
+});
+
+$('h1').click(function() {
+  switchThemes();
+});
+
+function switchThemes() {
+
+}
+
+function buildDoubles() {
+
+}
+
+
+//store elements in variables
+var mask = document.querySelector('svg'),
+  paragraph = document.querySelector('.aligner'),
+  btn = document.querySelector('h1');
+
+//add event listener to animate button to add the relevant CSS classes to the appropriate elements to animate them
+btn.addEventListener('click', function(e) {
+  e.preventDefault();
+  mask.classList.add('animate');
+  paragraph.classList.add('visible');
+}, false);
+
+//remove animation classes
+function removeAnimation(elem, animClass) {
+  elem.classList.remove(animClass);
+}
+mask.addEventListener('animationend', function() {
+  removeAnimation(this, 'animate');
+}, false);
+
+//remove class that transitions the paragraph after transition ends
+paragraph.addEventListener('transitionend', function() {
+  removeAnimation(this, 'visible');
+}, false);
